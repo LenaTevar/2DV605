2a17ef2e4acce18bfe3c331fa72cb1f03cf07064
Lena Tevar <13490448+LenaTevar@users.noreply.github.com>
2019-10-04 14:32:52 +0200

    Added double for loop to make life easier

--- CalcPI/CalcPI/Source.c
+++ CalcPI/CalcPI/Source.c
@@ -24,19 +24,29 @@ void calcPI(int iterations, int threads) {
 	mypi *= m;
 	double end = omp_get_wtime();
 	difTime = end - start;
-
+	
 	printf("Iterations %d - threads %d\tExecution time %f\n", iterations, threads, difTime);
 	printf("\tMyPI = %.70f\n", mypi);
 	printf("\tMyPI - PI = %.70f\n\n", (mypi - PI));
 	
+	
 }
+
+
 
 int main(int argc, char* argv[])
 {
-	int iterations = 1000000000;
+	int iteLength = 3;
 	int iteArr[3] = { 24000000, 48000000, 96000000 };
-	int threads = 48;
-	for (int i = 0; i < 3; i++) {
-		calcPI(iteArr[i], threads);
+
+	int thLength = 5;
+	int threads[5] = {1,6,12,24,48};
+
+	for (int thIndex = 0; thIndex < thLength; thIndex++)
+	{
+		for (int iteIndex = 0; iteIndex < 3; iteIndex++) {
+			calcPI(iteArr[iteIndex], threads[thIndex]);			
+		}
 	}
+	
 }
