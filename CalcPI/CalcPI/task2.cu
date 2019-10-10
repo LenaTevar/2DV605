eede291bafd58ccaee1bd239bb3908efd0293536
Lena Tevar <13490448+LenaTevar@users.noreply.github.com>
2019-10-10 15:02:27 +0200

    I have no idea what I'm doing

--- CalcPI/CalcPI/task2.cu
+++ CalcPI/CalcPI/task2.cu
@@ -21,25 +21,35 @@ __global__ void calcPi(int *iter, double *result){
 int main (void) {
 	printf("START\n");
 	int arrLength = 3;
-	int iteArr[3] = { 240, 480, 960 };
+	int iteArr[3] = { 24000000, 48000000, 94000000 };
 	double myResults[3];
 
 	int *d_ite, ite;
 	double *d_result, h_result;
 	int sizeInt = sizeof(int);
 	double sizeDou = sizeof(double);
+	
 
 /*
- * Allocate space for device copies
+ * Allocate space for device copies. 
+ * @params (void**) pointer, size
  */
 	cudaMalloc((void**) &d_ite, sizeInt);
 	cudaMalloc((void**) &d_result, sizeDou);
 
+	float time;
+	cudaEvent_t start, stop;
+	
 for(int j = 0; j < arrLength; j++){
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
+
 	printf("Running %d j: %d\n", iteArr[j], j);
 	ite = iteArr[j];
 /*
 * Copy input
+* @params (pointer dst, pointer src, size, type)
 */
 	cudaMemcpy(d_ite, &ite, sizeInt, cudaMemcpyHostToDevice);
 	//cudaMemcpy(d_result, &result, sizeDou , cudaMemcpyHostToDevice);
@@ -49,6 +59,7 @@ for(int j = 0; j < arrLength; j++){
 	calcPi<<<1,1>>>(d_ite, d_result);
 /*
 * Copy result back to host
+* @params (pointer dst, pointer src, size, type)
 */
 	cudaMemcpy(&h_result, d_result, sizeDou, cudaMemcpyDeviceToHost);
 	//printf("result %.70f\n", h_result);
@@ -56,11 +67,18 @@ for(int j = 0; j < arrLength; j++){
     myResults[j] = h_result;
     
 	//printf("mypi %f\n", mypi[j]);
-	
-/*
-* Clean up
-*/
-cudaFree(d_ite); cudaFree(d_result);
+		
+	/*
+	* Clean up
+	*/	
+	cudaFree(d_ite); cudaFree(d_result);
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&time, start, stop);
+	time /= 1000;
+	printf("Time to generate:  %f s \n", time);
+
+
 
 
 }//for loop
@@ -75,3 +93,6 @@ return 0;
 
 
 }
+/*
+* Notes: cuda uses pointers. 
+*/
